13b38abb02707e8952572523cddfcb28ee405a1d
recel@loveactually.com.au <recel@loveactually.com.au>
2018-01-07 15:04:17 +1100

    updated angular and typescript files + general cleanup

--- Web.UI/ClientApp/app/app.shared.module.ts
+++ Web.UI/ClientApp/app/app.shared.module.ts
@@ -3,21 +3,16 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { RouterModule } from '@angular/router';
-
 import { AppComponent } from './components/app/app.component';
 import { NavMenuComponent } from './components/navmenu/navmenu.component';
 import { HomeComponent } from './components/home/home.component';
-import { FetchDataComponent } from './components/fetchdata/fetchdata.component';
-import { CounterComponent } from './components/counter/counter.component';
-
+ 
 import { SearchDataComponent } from './components/search/search.component';
 
 @NgModule({
     declarations: [
         AppComponent,
         NavMenuComponent,
-        CounterComponent,
-        FetchDataComponent,
         HomeComponent,
         SearchDataComponent,
     ],
@@ -28,8 +23,6 @@ import { SearchDataComponent } from './components/search/search.component';
         RouterModule.forRoot([
             { path: '', redirectTo: 'home', pathMatch: 'full' },
             { path: 'home', component: HomeComponent },
-            { path: 'counter', component: CounterComponent },
-            { path: 'fetch-data', component: FetchDataComponent },
             { path: 'search-data', component: SearchDataComponent },
             { path: '**', redirectTo: 'home' }
         ])
